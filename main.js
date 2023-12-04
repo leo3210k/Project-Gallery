347abe697dcea4c88d2c265a7d22a50963bdf60d
Leonardo Coelho <leocoelho.pi@gmail.com>
2023-12-04 20:01:07 -0300

    set the structure of the main image

---
+++ main.js
@@ -0,0 +1,11 @@
+(() => {
+  const images = [
+    "https://cursinhoparamedicina.com.br/wp-content/uploads/2022/10/Paisagem-1.jpg",
+    "https://thumbs.dreamstime.com/b/belas-fotos-rosa-e-roxo-no-infravermelho-de-paisagens-rurais-na-europa-lindas-infravermelhos-roxos-cor-alemanha-157831242.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU-qQTiRaP8QN13CGPfT9bcU-2ZBJ3yKi0mw&usqp=CAU",
+    "https://blog.solo.ind.br/wp-content/uploads/2021/05/Jiuzhaigou.jpg",
+  ]
+
+  const main_content = document.querySelector(".image")
+  main_content.src = images[0]
+})()
